1cb46840dff08f5fb96178dd3e22d56d40ddef70
Johnny Cochrane <johnny@rotorstudios.com>
2015-12-07 01:06:48 +1100

    moved to ecosystem repo

--- bin/ecosystem-init.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-set python_exe=S:\python\Python27\python.exe
-set pythonw_exe=S:\python\Python27\pythonw.exe
-
-for %%d in (%~dp0..) do set ECO_ROOT=%%~fd
-
-set ECO_ENV=%ECO_ROOT%\env
-set PATH=%PATH%;%ECO_ROOT%\bin;S:\dev_area\Utilities\unxUtils
