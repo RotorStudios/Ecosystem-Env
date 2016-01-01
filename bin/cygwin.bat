3d2ad135736712a5f4a7d800ec84ee1204c6d90b
Johnny Cochrane <johnny@rotorstudios.com>
2016-01-01 11:49:34 +1100

    bash opens in eocosystem bin

--- bin/cygwin.bat
+++ bin/cygwin.bat
@@ -2,8 +2,12 @@
 
 call %~dp0_eco\_ecosystem-init.bat
 :: set $HOME dir for cygwin
-set HOME=%RS_USER_home%/cygwin64
-chdir S:\dev\auxiliary\apps\cygwin64\bin
+set HOME=%RS_USER_home%\cygwin64
 
-bash --login -i
+set SHELL=/usr/bin/bash
+set CHERE_INVOKING=1
+set ECO_HOME=%ECO_ROOT%/ecosystem-env/bin
+chdir %RS_AUXILIARY%\apps\cygwin64\bin
 
+bash --login -i -c 'cd ${ECO_HOME}; exec bash'
+::%RS_AUXILIARY%\apps\cygwin64\bin\mintty -c `cd ${ECO_HOME}; exec /bin/bash` -l
