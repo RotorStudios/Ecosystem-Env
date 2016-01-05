5a4488492897d6fa486f909d0ecf9976972e929a
Johnny Cochrane <johnny@rotorstudios.com>
2016-01-05 15:30:29 +1100

    added nuke support

---
+++ bin/dev-nuke9.0v1.bat
@@ -0,0 +1,6 @@
+@echo off&setlocal
+call %~dp0_eco\_ecosystem-init.bat
+title %~0
+echo launching: %~0
+
+call %eco_run_w% -r _launch-nuke.bat -t base,nuke9.0v1,dev
---
+++ bootstrap/_launch-nuke.bat
@@ -0,0 +1,1 @@
+start nuke%NUKE_MAJOR_VERSION%
