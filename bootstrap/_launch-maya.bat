6fb6a9937d5c2b2f43ed12494a46507d05b4e5f4
Johnny Cochrane <johnny@rotorstudios.com>
2016-01-05 21:31:50 +1100

    Commented out checks for existence of personal maya_app_dir and octane license dependencies as this will be handled by IT.

--- bootstrap/_launch-maya.bat
+++ bootstrap/_launch-maya.bat
@@ -4,27 +4,29 @@ SETLOCAL
 set _maya_app_dir_src= %ECO_ROOT%\ecosystem-env\utilities\maya_app_dir-template\%MAYA_ver%-x64
 set _maya_app_dir=%RS_USER_HOME_maya%\%MAYA_ver%-x64
 
-set _OCTANE_LIC_SRC=C:\OctaneLic
-set _OCTANE_ACCOUNT_CFG=octane_render_v1_live_account.cfg
+set _octane_lic_src=C:\OctaneLic
+set _octane_account_cfg=octane_render_v1_live_account.cfg
 
-if not exist %_MAYA_APP_DIR% (
-	robocopy %_MAYA_APP_DIR_SRC% %_MAYA_APP_DIR% /IS /E /Z
+
+if not exist %_maya_app_dir% (
+	robocopy %_maya_app_dir_src% %_maya_app_dir% /IS /E /Z
 )
 
-if exist %_OCTANE_LIC_SRC%\mayapref (
-	if not exist %_maya_app_dir%prefs\oiprefs.bin (
-		robocopy  %_OCTANE_LIC_SRC%\mayapref  %_MAYA_APP_DIR_SRC%\prefs\  *.bin  /IS
-	)
+::if exist %_octane_lic_src%\mayapref (
+::	if not exist %_maya_app_dir%prefs\oiprefs.bin (
+::		robocopy  %_octane_lic_src%\mayapref  %_maya_app_dir%\prefs\ *.bin  /IS
+::	)
+::
+::	if not exist %USERPROFILE%\AppData\Roaming\OctaneRender\%_octane_account_cfg% (
+::		robocopy %_octane_lic_src%\appdata_roaming^
+::		%USERPROFILE%\AppData\Roaming\OctaneRender\^
+::		%_octane_account_cfg% /IS
+::	)
+::)
 
-	if not exist %USERPROFILE%\AppData\Roaming\OctaneRender\%_OCTANE_ACCOUNT_CFG% (
-		robocopy %_OCTANE_LIC_SRC%\appdata_roaming^
-		%USERPROFILE%\AppData\Roaming\OctaneRender\^
-		%_OCTANE_ACCOUNT_CFG% /IS
-	)
-)
 
-if exist %_MAYA_APP_DIR% (
-	echo %_MAYA_APP_DIR% exists
+if exist %_maya_app_dir% (
+	echo %_maya_app_dir% exists
 )
 
 
