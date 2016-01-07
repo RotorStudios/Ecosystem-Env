bc9bd7592950c6cb2d594d7c2c473a84698658db
Johnny Cochrane <johnny@rotorstudios.com>
2016-01-07 13:04:27 +1100

    fixed path refactoring

--- bootstrap/_studio-init.bat
+++ bootstrap/_studio-init.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
-set "VIRTUAL_ENV=%RS_SW%\apps\python\py2.7.9-win64"
-call %VIRTUAL_ENV%\Scripts\activate.bat
-
 set "RS_SW=S:\dev\_S\sw"
 set "RS_USER_home=T:\users\%USERNAME%"
 set "RS_USER_DEV_home=S:\dev\home\%USERNAME%"
+
+set "VIRTUAL_ENV=%RS_SW%\apps\python\py2.7.9-win64"
+call %VIRTUAL_ENV%\Scripts\activate.bat
