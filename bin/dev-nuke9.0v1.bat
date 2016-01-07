5b6f049001e4813edfce431fa5021f09ec424f08
Johnny Cochrane <johnny@rotorstudios.com>
2016-01-07 12:26:31 +1100

    updated paths in preperation of next refactor

--- bin/dev-nuke9.0v1.bat
+++ bin/dev-nuke9.0v1.bat
@@ -3,4 +3,4 @@ call %~dp0_eco\_ecosystem-init.bat
 title %~0
 echo launching: %~0
 
-call %eco_run_w% -r _launch-nuke.bat -t base,nuke9.0v1,dev
+call %eco_run_w% -r _launch-nuke.bat -t base,nuke9.0v1,dev,deadline
