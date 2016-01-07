da8d5e66178ff66d0eef6fa6a7de9956daf8a4f2
Johnny Cochrane <johnny@rotorstudios.com>
2016-01-07 13:49:43 +1100

    Revert "rollback - accidently clobbered previous wanted change"
    This reverts commit 1d144503d91759a306e6fd76509ef5f7ba2f403f.

--- bin/_eco/_ecosystem-init.bat
+++ bin/_eco/_ecosystem-init.bat
@@ -4,17 +4,17 @@
 for %%d in (%~dp0..\..\..) do set ECO_ROOT=%%~fd
 echo ECO_ROOT=%ECO_ROOT%
 
-set eco_run=python %ECO_ROOT%\eco\bin\ecosystem.py
-set eco_run_w=pythonw %ECO_ROOT%\eco\bin\ecosystem.py
+set eco_run=python %ECO_ROOT%\ecosystem\bin\ecosystem.py
+set eco_run_w=pythonw %ECO_ROOT%\ecosystem\bin\ecosystem.py
 
 Doskey eco=%eco_run% $*
 
-set ECO_ENV=%ECO_ROOT%\eco-env\env
+set ECO_ENV=%ECO_ROOT%\ecosystem-env\env
 echo ECO_ENV=%ECO_ENV%
 
 set PATH=%PATH%;^
-%ECO_ROOT%\eco\bin;^
-%ECO_ROOT%\eco-env\bin;^
-%ECO_ROOT%\eco-env\bootstrap
+%ECO_ROOT%\ecosystem\bin;^
+%ECO_ROOT%\ecosystem-env\bin;^
+%ECO_ROOT%\ecosystem-env\bootstrap
 
 call _studio-init.bat
