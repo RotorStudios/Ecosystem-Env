366d28ac9448c29b5eae24a647a21cbb26cfefd0
Johnny Cochrane <johnny@rotorstudios.com>
2016-01-11 21:57:30 +1100

    added verbosity added feedback for path validations

--- bin/_eco/_ecosystem-init.bat
+++ bin/_eco/_ecosystem-init.bat
@@ -1,20 +1,24 @@
 @ECHO OFF
 
 
+echo.
+echo root@ %~dp0...
+echo.&&echo  @%~nx0
+
+
 for %%d in (%~dp0..\..\..) do set ECO_ROOT=%%~fd
-echo ECO_ROOT=%ECO_ROOT%
+set           "ECO_ENV=%ECO_ROOT%\eco-env\env"
+set    "eco_run=python %ECO_ROOT%\eco\bin\ecosystem.py"
+set "eco_run_w=pythonw %ECO_ROOT%\eco\bin\ecosystem.py"
 
-set eco_run=python %ECO_ROOT%\eco\bin\ecosystem.py
-set eco_run_w=pythonw %ECO_ROOT%\eco\bin\ecosystem.py
-
-Doskey eco=%eco_run% $*
-
-set ECO_ENV=%ECO_ROOT%\eco-env\env
-echo ECO_ENV=%ECO_ENV%
+DOSKEY eco=%eco_run% $*
 
 set PATH=%PATH%;^
 %ECO_ROOT%\eco\bin;^
 %ECO_ROOT%\eco-env\bin;^
 %ECO_ROOT%\eco-env\bootstrap
+
+echo   ECO_ROOT= "%ECO_ROOT%"
+echo    ECO_ENV= "%ECO_ENV%"
 
 call _studio-init.bat
--- bootstrap/_studio-init.bat
+++ bootstrap/_studio-init.bat
@@ -1,8 +1,36 @@
 @ECHO OFF
+echo.
+echo  @%~0
+
 
 set "RS_SW=S:\dev\_S\sw"
 set "RS_USER_home=T:\users\%USERNAME%"
 set "RS_USER_DEV_home=S:\dev\_S\dev-area\home\%USERNAME%"
-
 set "VIRTUAL_ENV=%RS_SW%\apps\python\py2.7.9-win64"
-call %VIRTUAL_ENV%\Scripts\activate.bat
+call :check_path %RS_SW%
+call :check_path %RS_USER_home%
+call :check_path %RS_USER_DEV_home%
+call :check_path %VIRTUAL_ENV%\Scripts\activate.bat
+
+goto End
+
+
+
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:check_path     -- validates if path exists 
+::              -- %~1: path to validate
+SETLOCAL
+  set "_path=%~1"
+  if not exist %_path% (
+    echo    ! Path invalid =^( - "%_path%"
+    echo.
+    pause && exit 1
+  ) else (
+          echo    - is valid :D - "%_path%"
+  )
+ENDLOCAL
+goto:eof
+
+echo.
+:End
