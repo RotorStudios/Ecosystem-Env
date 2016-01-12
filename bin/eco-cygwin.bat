f7f2a009dee0db76c2f5630de5a9cf6726243569
Johnny Cochrane <johnny@rotorstudios.com>
2016-01-12 11:56:37 +1100

    added verbosity to cygwin launcher cleaned up env pointers

--- bin/eco-cygwin.bat
+++ bin/eco-cygwin.bat
@@ -6,8 +6,37 @@ set HOME=%RS_USER_home%\cygwin64
 
 set SHELL=/usr/bin/bash
 set CHERE_INVOKING=1
-set ECO_HOME=%ECO_ROOT%/ecosystem-env/bin
-chdir %RS_AUXILIARY%\apps\cygwin64\bin
+set ECO_HOME=%ECO_ROOT%\eco-env\bin
 
+
+call :check_path RS_USER_home  %RS_USER_home%
+call :check_path HOME  %HOME%
+call :check_path ECO_HOME  %ECO_HOME%
+
+
+chdir %RS_SW%\cygwin64\bin
 bash --login -i -c 'cd ${ECO_HOME}; exec bash'
-::%RS_AUXILIARY%\apps\cygwin64\bin\mintty -c `cd ${ECO_HOME}; exec /bin/bash` -l
+::%RS_SW%\cygwin64\bin\mintty -c `cd ${ECO_HOME}; exec /bin/bash` -l
+
+
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:check_path     -- validates if path exists 
+::              -- %~1: env name
+::              -- %~2: path to validate
+SETLOCAL
+  set "_env_name=%~1"
+  set "_path=%~2"
+  if not exist %_path% (
+    echo    ! %_env_name% Path invalid =^( - "%_path%"
+    echo.
+    pause && exit 1
+  ) else (
+          echo    - %_env_name%= "%_path%"
+  )
+ENDLOCAL
+goto:eof
+
+
+echo.
+:End
--- bootstrap/_studio-init.bat
+++ bootstrap/_studio-init.bat
@@ -8,11 +8,11 @@ set "RS_USER_home=T:\users\%USERNAME%"
 set "RS_USER_DEV_home=S:\dev\_S\dev-area\home\%USERNAME%"
 set "VIRTUAL_ENV=%RS_SW%\apps\python\py2.7.9-win64"
 
-call :check_path %RS_SW%
-call :check_path %RS_USER_home%
-call :check_path %RS_USER_DEV_home%
-call :check_path %VIRTUAL_ENV%\Scripts\activate.bat
-call :check_path %RS_SW%\chocolatey\bin
+call :check_path RS_SW  %RS_SW%
+call :check_path RS_USER_home  %RS_USER_home%
+call :check_path RS_USER_DEV_home  %RS_USER_DEV_home%
+call :check_path virtual_env/script  %VIRTUAL_ENV%\Scripts\activate.bat
+call :check_path chocolatey  %RS_SW%\chocolatey\bin
 
 set "PATH=%PATH%;%RS_SW%\chocolatey\bin"
 %VIRTUAL_ENV%\Scripts\activate.bat
@@ -23,20 +23,25 @@ goto End
 
 
 
+
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :check_path     -- validates if path exists 
-::              -- %~1: path to validate
+::              -- %~1: env name
+::              -- %~2: path to validate
 SETLOCAL
-  set "_path=%~1"
+  set "_env_name=%~1"
+  set "_path=%~2"
   if not exist %_path% (
-    echo    ! Path invalid =^( - "%_path%"
+    echo    ! %_env_name% Path invalid =^( - "%_path%"
     echo.
     pause && exit 1
   ) else (
-          echo    - is valid :D - "%_path%"
+          echo    - %_env_name%= "%_path%"
   )
 ENDLOCAL
 goto:eof
+
+
 
 echo.
 :End
