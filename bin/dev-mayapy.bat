0bca93a801d54f890eefdfe3d9c54b8d5393e617
johnco <johnny@rotorstudios.com>
2016-01-14 22:03:00 +1100

    using absolute maya env

--- bin/dev-mayapy.bat
+++ bin/dev-mayapy.bat
@@ -3,4 +3,4 @@ call %~dp0_eco\_ecosystem-init.bat
 title %~0
 echo launching: %~0
 
-call %eco_run_w% -r mayapy -t maya2015,dev,base
+call %eco_run_w% -r mayapy -t maya,dev,base
--- bin/exp-maya-assetlib.bat
+++ bin/exp-maya-assetlib.bat
@@ -3,4 +3,4 @@ call %~dp0_eco\_ecosystem-init.bat
 title %~0
 echo launching: %~0
 
-call %eco_run_w% -r launch-maya.bat -t base,maya2015,mayaportable,assetlib
+call %eco_run_w% -r launch-maya.bat -t base,maya,mayaportable,assetlib
